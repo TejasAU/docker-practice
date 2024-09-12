a357525ab7cc8da17b562e0959b608b3ccc25ce3
tejas <tejas0418@gmail.com>
2024-09-12 10:12:54 +0530

    updated color

--- CodeGym-main/Part 3 UI Code/app.py
+++ CodeGym-main/Part 3 UI Code/app.py
@@ -89,9 +89,9 @@ def delete_from_cart():
     return jsonify(cart[user_id])
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
     
-#if __name__ == '__main__':
-#    app.run(host='0.0.0.0', debug=True)
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', debug=True)
 
